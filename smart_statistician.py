476f324d0d1d6909219f492e713ee0690d82a696
Abhishek CM <74650961+abekcm20@users.noreply.github.com>
2021-04-24 21:06:50 +0530

    Update smart_statistician.py

--- smart_statistician.py
+++ smart_statistician.py
@@ -1,5 +1,4 @@
 import csv
-import statistics
 Active = None
 data = {}
 lst = []
@@ -16,43 +15,73 @@ class Smart_Statistician:
         temp = self.dataset[oldname]
         del self.dataset[oldname]
         self.dataset[newname] = temp
-        print('successfully change Rainfall To python\n')
+        print('\nsuccessfully change Rainfall To python\n')
     def sort_list(self,key_rename):
-        post_sort_lst = sorted(self.dataset[key_rename])
+        post_sort_lst = ([int(n) for n in self.dataset[key_rename]])
+        for i in range (len(post_sort_lst)):
+            for j in range(i + 1, len(post_sort_lst)):
+                if(post_sort_lst[i] > post_sort_lst[j]):
+                    temp1 = post_sort_lst[i]
+                    post_sort_lst[i] = post_sort_lst[j]
+                    post_sort_lst[j] = temp1
         print(post_sort_lst)
     def Analyze(self,key_Analyze):
-        temp_list = self.dataset[key_Analyze]
-        for i in range(0, len(temp_list)):
-            temp_list[i] = int(temp_list[i])
-        print("number of values (n): ",len(temp_list))
+        temp_list = [int(n) for n in self.dataset[key_Analyze]]
+        mean = 0
+        len_num = len(temp_list)
+        for m in temp_list:
+            mean += m
+        mean = mean/len_num
+        
+        
+        temp_list.sort()
+        if len_num % 2 == 0:
+            median1 = temp_list[len_num//2]
+            median2 = temp_list[len_num//2 - 1]
+            median = (median1 + median2)/2
+        else:
+            median = temp_list[len_num//2]
+            
+        max_count = (0,0)
+        for max_num in temp_list:
+            occur = temp_list.count(max_num)
+            if occur > max_count[0]:
+                max_count = (occur, max_num)
+        
+        variance = sum([((x - mean) ** 2) for x in temp_list]) / len(temp_list)
+        stdeV = variance ** 0.5
+    
+        
+        print("\nnumber of values (n): ",len(temp_list))
         print("             minimum: ",min(temp_list))
         print("             maximum: ",max(temp_list))
-        print("                Mean: ",statistics.mean(temp_list))
-        print("              Median: ",statistics.median(temp_list))
-        print("                Mode: ",statistics.mode(temp_list))
-        print("  Standard deviation: ",round(statistics.stdev(temp_list),2))   
+        print("                Mean: ",round(mean,2))
+        print("              Median: ",round(median,2))
+        print("                Mode: ",max_count[1])
+        print("  Standard deviation: ",round(stdeV,2))   
             
             
         
 def printfun():
     if Active is None:
-        print("Welcome to The Smart Statistician! \nProgrammed by Abhishek CM")
-    print("Please choose from the following options:    \n1 - Load data from a file    \n2 - Display the data to the screen    \n3 - Rename a set    \n4 - Sort a set    \n5 - Analyze a set    \n6 - Quit")
+        print("\nWelcome to The Smart Statistician! \nProgrammed by Abhishek CM")
+    print("\nPlease choose from the following options:    \n1 - Load data from a file    \n2 - Display the data to the screen    \n3 - Rename a set    \n4 - Sort a set    \n5 - Analyze a set    \n6 - Quit")
     
     
     
     
 def printlist(num):
     if num == 3:
-        print("Which set do you want to rename?")
+        print("\nWhich set do you want to rename?")
     elif num == 4:
-        print("Which set do you want to sort?")
+        print("\nWhich set do you want to sort?")
     elif num == 5:
-        print("Which set do you want to Analyze?")
+        print("\nWhich set do you want to Analyze?")
     for keys in data:
         lst.append(keys)
     for j in lst:
         print((lst.index(j)+1), '-', j)
+    print('\n select one number\n')
     n = int(input())-1
     return n
 
@@ -66,9 +95,17 @@ while(Active !='Quit'):
     if options == 1:
         Active = not None
     if Active is None or options > 6:
-        print("please Load data from a file or Invalid option!! Please choose any among 1 to 6./n")
+        if options == 6:
+            Active='Quit'
+        elif options < 6:
+            print("\nplease Load data from a file\n")
+        else:
+            print("\nInvalid option!! Please choose any among 1 to 6.\n")
     else:
         if options == 1:
+            if bool(data):
+                data.clear()
+            print("\n Enter the file name with extension\n")
             with open(input(),'r') as ifile:
                 dataReader = csv.reader(ifile)
                 for line in dataReader:
@@ -78,7 +115,7 @@ while(Active !='Quit'):
             operation.Display_Data()
         elif options == 3:
             return_num_3 = printlist(options)
-            new = input()
+            new = input("\nEnter the new name\n")
             operation.Rename(lst[return_num_3],new)
             lst.clear()
         elif options == 4:
@@ -90,5 +127,4 @@ while(Active !='Quit'):
             operation.Analyze(lst[return_num_5])
             lst.clear()
         elif options == 6:
-            print('Thanks')
             Active='Quit'
